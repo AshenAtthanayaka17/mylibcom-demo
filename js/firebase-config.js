932c522b58a09736126342b7127f4b807bfa3d06
Administration <156107869+AshenAtthanayaka17@users.noreply.github.com>
2025-12-13 05:04:27 +0000

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,15 +1,14 @@
-export function initFirebase() {
-    const firebaseConfig = {
-        apiKey: "AIzaSyBH6LVGPFkfx7NVUtGCLvvCC9MXcdZo7zk",
-        authDomain: "mylibcom-co.firebaseapp.com",
-        projectId: "mylibcom-co",
-        storageBucket: "mylibcom-co.firebasestorage.app",
-        messagingSenderId: "922343336014",
-        appId: "1:922343336014:web:5d71dfadf665c9a6c73b1f",
-        measurementId: "G-DCG0KW3N11"
-    };
-
+const firebaseConfig = {
+  apiKey: "AIzaSyBH6LVGPFkfx7NVUtGCLvvCC9MXcdZo7zk",
+  authDomain: "mylibcom-co.firebaseapp.com",
+  projectId: "mylibcom-co",
+  storageBucket: "mylibcom-co.firebasestorage.app",
+  messagingSenderId: "922343336014",
+  appId: "1:922343336014:web:5d71dfadf665c9a6c73b1f",
+  measurementId: "G-DCG0KW3N11"
+};
     firebase.initializeApp(firebaseConfig);
     window.auth = firebase.auth();
     window.db = firebase.firestore();
+
 }
